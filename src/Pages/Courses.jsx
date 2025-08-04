3de4a5b137d02537b38dfdd57f42ba03e8f18e4a
spystar20 <hert5197@gmail.com>
2025-08-04 08:08:02 +0530

    filter

--- src/Pages/Courses.jsx
+++ src/Pages/Courses.jsx
@@ -3,6 +3,7 @@ import { FaSearch, FaStar } from "react-icons/fa";
 import { MdOutlineKeyboardArrowDown } from 'react-icons/md';
 import Course from '../data/course'
 import courseCategories from '../data/CourseCategories';
+
 const Courses = () => {
 const [openCategory ,setopenCategory ] = useState(false)
 const toggleCategory = (name) =>{ 
@@ -12,6 +13,11 @@ const toggleCategory = (name) =>{
      }))
 }
 
+// const [filterCourse , setfilterCourse] =useState('')
+// setfilterCourse(() =>{
+//   Course.filter(course => {
+//   })
+// })
   return (
     <div className=' bg-white w-full h-[100vh] font-[Roboto]'>
 <div className='w-full h-[30%] flex flex-col gap-3 justify-center items-center text-white  home-bg'>
@@ -43,7 +49,7 @@ const toggleCategory = (name) =>{
 <div onClick={() =>toggleCategory(course.category)} className='flex justify-between items-center pt-2 '>   
 <label>
     <span className='flex justify-center items-center'>
-    <input type="checkbox" className='w-3 cursor-pointer border-none h-3 accent-pink-400' name={course.category} id="" />
+    <input  type="checkbox" className='w-3 cursor-pointer border-none h-3 accent-pink-400' name={course.category} id="" />
     <span key={index} className='text-xl px-3 text-wrap capitalize '>{course.category}</span>
     </span>
 </label>
@@ -109,6 +115,38 @@ const toggleCategory = (name) =>{
             ))}
           </div>
         </div>
+          {/* temp */}
+             <div className='flex flex-col cursor-pointer shadow-2xl bg-white w-full gap-3 rounded-2xl   h-full px-6 py-6'>
+              <div className='grid group '>
+                <div className='col-start-1 row-start-1 z-0'><img src="https://i.pinimg.com/736x/42/da/db/42dadbfe747110c544fab569d954869c.jpg" className=' col-span-1 rounded-2xl' alt="" /></div>
+                <div className='col-start-1 invisible group-hover:visible  px-3 py-3 flex  justify-between items-end row-start-1 z-10 bg-blue-300 opacity-85  rounded-2xl'>
+                  <div >
+                    <button className='flex justify-center items-center  transition-all hover:scale-95 scale-100 group   border-blue-300 font-[Comic_Relief]  cursor-pointer bg-pink-400 text-white rounded-lg py-1 px-2 gap-1 text-lg  capitalize font-medium'>view course</button></div>
+                  <div>
+                    <button className='flex justify-center items-center  transition-all hover:scale-95 scale-100 group    font-[Comic_Relief]  cursor-pointer bg-pink-400 text-white rounded-lg py-1 px-2 gap-1 text-lg  capitalize font-medium'>Free</button></div>
+                </div>
+              </div>
+              <div className='flex justify-start items-center gap-3'>
+                <div><img src="https://i.pinimg.com/736x/26/f7/b9/26f7b9e4919314c77cf658313c59c0e8.jpg" className='w-16 rounded-full ' alt="" /></div>
+                <div className='flex flex-col justify-start items-start capitalize font-[outfit]'>
+                  <span className='text-lg font-medium'>cha eun woo </span>
+                  <span className='font-medium text-sm '>instructor</span>
+                </div>
+              </div>
+              <div className='flex flex-col text-left  flex-wrap'>
+                <h2 className='text-xl font-semibold'>UI/UX Design Masterclass </h2>
+                <p className='text-lg text-wrap'> Master design Lorem ipsum dolor sit. tools & principles to craft stunning user experiences..</p>
+              </div>
+              <div className='flex  justify-between  items-center'>
+                <div className='flex border capitalize bg-black text-white text-sm font-medium  rounded-lg px-2 py-2 justify-start items-center gap-2'>
+                  <span className='flex justify-center text-yellow-500'><FaStar /></span>
+                  <h2 className=' font-medium'> 4.8</h2>
+                </div>
+                <div className='flex border capitalize bg-black text-white text-sm font-medium  rounded-lg px-2 py-2 justify-start items-center gap-3'>
+                  12.9k rating
+                </div>
+              </div>
+            </div>
 </div>
     </div>
   )

5e72d37ce27f563cfe731594d0f87080b5c81061
spystar20 <hert5197@gmail.com>
2025-08-11 22:17:57 +0530

    filter

--- src/Pages/Courses.jsx
+++ src/Pages/Courses.jsx
@@ -12,8 +12,8 @@ import { CiHeart } from 'react-icons/ci';
 
 const Courses = () => {
   // toggling courseCategories
-  const [openCourseCategories , setOpenCourseCategories] =useState(false)
-  const togglecourseCategory =() =>{
+  const [openCourseCategories, setOpenCourseCategories] = useState(false)
+  const togglecourseCategory = () => {
     setOpenCourseCategories(!openCourseCategories)
   }
   //  toggling subcategories
@@ -25,15 +25,18 @@ const Courses = () => {
     }))
   }
   //  toggle like
-  const [Liked,setLiked] = useState(true)
-  const handleLike =() =>{
+  const [Liked, setLiked] = useState(true)
+  const handleLike = () => {
     setLiked(!Liked)
   }
-  // const [filterCourse , setfilterCourse] =useState('')
-  // setfilterCourse(() =>{
-  //   Course.filter(course => {
-  //   })
-  // })
+ const [filter , setfilter] = useState('')
+ const Update = (e) =>{
+  setfilter(e.target.value)
+ }
+ const FilteredArray = Course.filter(course => !filter || course.category === filter   
+ )
+console.log(FilteredArray)
+  
   return (
     <div className=' bg-white w-full h-[100vh] font-[Roboto]'>
       <div className='w-full h-[30%] flex flex-col gap-3 justify-center items-center text-white  home-bg'>
@@ -58,135 +61,159 @@ const Courses = () => {
       {/* filter */}
       <div className='flex items-start justify-between '>
         <div className='flex flex-col justify-start items-start border-r px-5 mt-1 w-[23%] '>
-        <div className=' font-[Outfit] '>
-          <h2  onClick={togglecourseCategory} className='text-2xl font-medium py-4 pb-5 flex justify-between items-center   '>Categories <span><MdOutlineKeyboardArrowDown className='text-4xl font-black' /></span></h2>
-          {openCourseCategories &&
-          courseCategories.map((course, index) => {
-         return   (
-        
-              <div key={index}>
+          <div className=' font-[Outfit] w-full  '>
+            <h2 onClick={togglecourseCategory} className='text-2xl font-medium py-4 pb-5 flex justify-between items-center   '>Categories <span><MdOutlineKeyboardArrowDown className={`text-4xl font-black rotate-0  transition-all cursor-pointer ease-out ${openCourseCategories ? 'rotate-180' : 'rotate-0'}`} /></span></h2>
+            {openCourseCategories &&
+              courseCategories.map((course, index) => {
+                return (
 
-                <div onClick={() => toggleCategory(course.category)} className='flex justify-between items-center pt-2 '>
-                  <label>
+                  <div key={index}>
+
+                    <div onClick={() => toggleCategory(course.category)} className='flex items-center pt-2 hide '>
+                      <label>
+                        <span className='flex justify-center items-center'>
+                          <input type="checkbox" className='w-3 cursor-pointer border-none h-3 accent-pink-400' name={course.category} onChange={Update} value={filter} id="" />
+                          <span className='text-xl px-3 text-wrap capitalize '>{course.category}</span>
+                        </span>
+                      </label>
+                      <span ><MdOutlineKeyboardArrowDown className='text-xl cursor-pointer ' /></span>
+                    </div>
+                    {openCategory[course.category] && course.subcategories && (
+
+
+                      <div className='pl-4'>
+                        {course.subcategories.map((subc, index) => {
+                          return (
+                            <div className='flex items-center  py-3 '>
+                              <label htmlFor="development">
+                                <span className='flex justify-center items-center'>
+                                  <input type="checkbox" className=' border-none  accent-pink-400' name="web development" id="" />
+                                  <span className='text-lg font-normal px-3 capitalize'>{subc}</span>
+                                </span>
+                              </label>
+                            </div>
+                          )
+                        })}
+                      </div>
+                    )}
+
+                  </div>
+
+
+                )
+              })}
+
+          </div>
+          {/* Rating */}
+          <div className='font-[Outfit]  w-full'>
+            <h2 className='text-2xl font-medium py-4 pb-5 flex justify-between items-center  capitalize '>rating<span><MdOutlineKeyboardArrowDown className='text-4xl font-black' /></span></h2>
+            <div>
+             
+              <div className='pl-4'>
+                <div className='flex items-center  py-3 '>
+                  <label htmlFor="4.5">
                     <span className='flex justify-center items-center'>
-                      <input type="checkbox" className='w-3 cursor-pointer border-none h-3 accent-pink-400' name={course.category} id="" />
-                      <span  className='text-xl px-3 text-wrap capitalize '>{course.category}</span>
+                      <input type="radio" className=' border-none  accent-pink-400' name="4.5" id="" />
+                      <span className='text-lg font-normal px-3 capitalize'>4.5 & above</span>
                     </span>
                   </label>
-                  <span ><MdOutlineKeyboardArrowDown className='text-xl cursor-pointer' /></span>
                 </div>
-                {openCategory[course.category] && course.subcategories && (
-
-
-                  <div className='pl-4'>
-                    {course.subcategories.map((subc, index) => {
-                      return (
-                        <div className='flex items-center  py-3 '>
-                          <label htmlFor="development">
-                            <span className='flex justify-center items-center'>
-                              <input type="checkbox" className=' border-none  accent-pink-400' name="web development" id="" />
-                              <span className='text-lg font-normal px-3 capitalize'>{subc}</span>
-                            </span>
-                          </label>
-                        </div>
-                      )
-                    })}
-                  </div>
-                )}
-
-              </div>
-            
-                
-            )
-          })}
-  
-        </div>
-
- <div className='font-[Outfit] px-5'>
-           <h2  className='text-2xl font-medium py-4 pb-5 flex justify-between items-center   '>rate <span><MdOutlineKeyboardArrowDown className='text-4xl font-black' /></span></h2>
-          {courseCategories.map((course, index) => {
-            return (
-              <div>
-
-                <div onClick={() => toggleCategory(course.category)} className='flex justify-between items-center pt-2 '>
-                  <label>
+                <div className='flex items-center  py-3 '>
+                  <label htmlFor="4.0">
                     <span className='flex justify-center items-center'>
-                      <input type="checkbox" className='w-3 cursor-pointer border-none h-3 accent-pink-400' name={course.category} id="" />
-                      <span key={index} className='text-xl px-3 text-wrap capitalize '>{course.category}</span>
+                      <input type="radio" className=' border-none  accent-pink-400' name="4.0" id="" />
+                      <span className='text-lg font-normal px-3 capitalize'>4.0 & above</span>
                     </span>
                   </label>
-                  <span ><MdOutlineKeyboardArrowDown className='text-xl cursor-pointer' /></span>
                 </div>
-                {openCategory[course.category] && course.subcategories && (
-
-
-                  <div className='pl-4'>
-                    {course.subcategories.map((subc, index) => {
-                      return (
-                        <div className='flex items-center  py-3 '>
-                          <label htmlFor="development">
-                            <span className='flex justify-center items-center'>
-                              <input type="checkbox" className=' border-none  accent-pink-400' name="web development" id="" />
-                              <span className='text-lg font-normal px-3 capitalize'>{subc}</span>
-                            </span>
-                          </label>
-                        </div>
-                      )
-                    })}
-                  </div>
-                )}
-
+                <div className='flex items-center  py-3 '>
+                  <label htmlFor="3.5">
+                    <span className='flex justify-center items-center'>
+                      <input type="radio" className=' border-none  accent-pink-400' name="3.0" id="" />
+                      <span className='text-lg font-normal px-3 capitalize'>3.5 & above</span>
+                    </span>
+                  </label>
+                </div>
               </div>
-            )
-          })}
-        </div>
+            </div>
+
+          </div>
+          {/* price */}
+          <div className='font-[Outfit]  w-full'>
+            <h2 className='text-2xl font-medium py-4 pb-5 flex justify-between items-center  capitalize '>price<span><MdOutlineKeyboardArrowDown className='text-4xl font-black' /></span></h2>
+            <div>
+              <div className='pl-4'>
+                <div className='flex items-center  py-1 '>
+                  <label htmlFor="free">
+                    <span className='flex justify-center items-center'>
+                      <input type="checkbox" className=' border-none  accent-pink-400' name="free" id="" />
+                      <span className='text-lg font-normal px-3 capitalize'>free</span>
+                    </span>
+                  </label>
+                </div>
+                </div>
+              <div className='pl-4'>
+                <div className='flex items-center  py-1 '>
+                  <label htmlFor="paid">
+                    <span className='flex justify-center items-center'>
+                      <input type="checkbox" className=' border-none  accent-pink-400' name="paid" id="" />
+                      <span className='text-lg font-normal px-3 capitalize'>paid</span>
+                    </span>
+                  </label>
+                </div>
+              </div>
+            </div>
+          </div>
+
         </div>
         {/* courses */}
         <div className='w-[80%]'>
           <div className='grid grid-cols-3  gap-4 px-5 py-10'>
-           {Course.map((course )=>{ return(
-        <div className='flex flex-col cursor-pointer shadow-2xl bg-white  w-full gap-3 rounded-2xl   h-full px-6 py-6'>
-          <div className='grid group '>
-            <div className='col-start-1 row-start-1 z-0'><img src={course.img} className='drop-shadow-xs col-span-1 rounded-2xl' alt="" /></div>
-            <div className='col-start-1 relative invisible group-hover:visible  px-3 py-3 flex  justify-between items-end row-start-1 z-10 bg-black  opacity-65  rounded-2xl'>
-              <div onClick={handleLike} className='absolute top-0 right-0  m-1 transition-all ease-out '>{Liked ? <CiHeart className='text-4xl text-pink-400' /> : <FaHeart className='text-4xl text-pink-400'/>}</div>
-              <div >
-                <button className='flex justify-center items-center  transition-all hover:scale-95 scale-100 group    font-[Comic_Relief]  cursor-pointer bg-pink-400 text-white rounded-lg py-1 px-2 gap-1 text-lg  capitalize font-medium'><span><FaShoppingCart /></span> <span>add to cart</span></button></div>
-              <div>
-                <button className='flex justify-center items-center  transition-all hover:scale-95 scale-100 group    font-[Comic_Relief]  cursor-pointer bg-pink-400 text-white rounded-lg py-1 px-2 gap-1 text-lg  capitalize font-medium'>{course.price}</button></div>
-            </div>
-          </div>
-          <div className='flex justify-start items-center gap-3'>
-            <div><img src={course.instructor_img} className='w-16 rounded-full ' alt={course.instructor_img} /></div>
-            <div className='flex flex-col justify-start items-start capitalize font-[outfit]'>
-              <span className='text-lg font-medium'>{course.instructor_name}</span>
-              <span className='font-medium text-sm '>instructor</span>
-            </div>
-          </div>
+            {FilteredArray.map((course) => {
+              return (
+                <div className='flex flex-col cursor-pointer shadow-2xl bg-white  w-full gap-3 rounded-2xl   h-full px-6 py-6'>
+                  <div className='grid group '>
+                    <div className='col-start-1 row-start-1 z-0'><img src={course.img} className='drop-shadow-xs col-span-1 rounded-2xl' alt="" /></div>
+                    <div className='col-start-1 relative invisible group-hover:visible  px-3 py-3 flex  justify-between items-end row-start-1 z-10 bg-black  opacity-65  rounded-2xl'>
+                      <div onClick={handleLike} className='absolute top-0 right-0  m-1 transition-all ease-out '>{Liked ? <CiHeart className='text-4xl text-pink-400' /> : <FaHeart className='text-4xl text-pink-400' />}</div>
+                      <div >
+                        <button className='flex justify-center items-center  transition-all hover:scale-95 scale-100 group    font-[Comic_Relief]  cursor-pointer bg-pink-400 text-white rounded-lg py-1 px-2 gap-1 text-lg  capitalize font-medium'><span><FaShoppingCart /></span> <span>add to cart</span></button></div>
+                      <div>
+                        <button className='flex justify-center items-center  transition-all hover:scale-95 scale-100 group    font-[Comic_Relief]  cursor-pointer bg-pink-400 text-white rounded-lg py-1 px-2 gap-1 text-lg  capitalize font-medium'>{course.price}</button></div>
+                    </div>
+                  </div>
+                  <div className='flex justify-start items-center gap-3'>
+                    <div><img src={course.instructor_img} className='w-16 rounded-full ' alt={course.instructor_img} /></div>
+                    <div className='flex flex-col justify-start items-start capitalize font-[outfit]'>
+                      <span className='text-lg font-medium'>{course.instructor_name}</span>
+                      <span className='font-medium text-sm '>instructor</span>
+                    </div>
+                  </div>
 
-          <div className='flex flex-col text-left  flex-wrap'>
-            <h2 className='text-xl font-semibold'>{course.course_name} </h2>
-            <p className='text-lg text-wrap line-clamp-2'> {course.course_desc}</p>
-          </div>
-       
-          <div className='flex  justify-between  items-center'>
-            <div className='flex border capitalize bg-black text-white text-sm font-medium  rounded-lg px-2 py-2 justify-start items-center gap-2'>
-              <span className='flex justify-center text-yellow-500'><FaStar /></span>
-              <h2 className=' font-medium'>{course.rating}</h2>
-            </div>
-            <div className='flex border capitalize bg-black text-white text-sm font-medium  rounded-lg px-2 py-2 justify-start items-center gap-3'>
-              <span><FaEye/></span><span>{course.view}</span>
-            </div>
-          </div>
-         <div className='flex justify-between items-center '><span className='flex items-center gap-2 text-lg capitalize '>< SiBookstack />{course.chapters} chapters</span><span className='flex items-center gap-2 text-lg capitalize '><IoTime />{course.duration}</span></div>
-        </div>
-)})}
+                  <div className='flex flex-col text-left  flex-wrap'>
+                    <h2 className='text-xl font-semibold'>{course.course_name} </h2>
+                    <p className='text-lg text-wrap line-clamp-2'> {course.course_desc}</p>
+                  </div>
+
+                  <div className='flex  justify-between  items-center'>
+                    <div className='flex border capitalize bg-black text-white text-sm font-medium  rounded-lg px-2 py-2 justify-start items-center gap-2'>
+                      <span className='flex justify-center text-yellow-500'><FaStar /></span>
+                      <h2 className=' font-medium'>{course.rating}</h2>
+                    </div>
+                    <div className='flex border capitalize bg-black text-white text-sm font-medium  rounded-lg px-2 py-2 justify-start items-center gap-3'>
+                      <span><FaEye /></span><span>{course.view}</span>
+                    </div>
+                  </div>
+                  <div className='flex justify-between items-center '><span className='flex items-center gap-2 text-lg capitalize '>< SiBookstack />{course.chapters} chapters</span><span className='flex items-center gap-2 text-lg capitalize '><IoTime />{course.duration}</span></div>
+                </div>
+              )
+            })}
           </div>
         </div>
 
 
-        </div>
-   
+      </div>
+
     </div>
   )
 }

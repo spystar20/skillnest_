cc951260ba5dd010340eed970ac85083f722f7a3
spystar20 <hert5179@gmail.com>
2026-03-20 23:28:13 +0530

    signup ui

--- src/AuthPages/Login.jsx
+++ src/AuthPages/Login.jsx
@@ -3,18 +3,51 @@ import login from '../assets/login.png'
 const Login = () => {
   return (
   
-    <div className='w-full max-h-full bg-black py-32 px-12'>
+    <div className='w-full bg-black py-32 px-32'>
       <div className='bg-white/85 grid grid-cols-2 p-6 rounded-2xl'>
       <div className='flex flex-col items-start justify-end box rounded-2xl p-12'>
 <div className='text-white flex flex-col gap-2 '>
   <h6 className='font-body font-semibold text-sm capitalize '>Lorem ipsum dolor sit amet.</h6>
-  <h2 className='font-heading font-bold text-3xl'>Lorem ipsum dolor sit amet Lorem, ipsum dolor.</h2>
+  <h2 className='font-heading font-bold text-5xl'>Lorem ipsum dolor sit amet Lorem, ipsum dolor.</h2>
 </div>
       </div>
-      <div className='flex flex-col p-32'>
-<div>
-  <h2>welcome to skillnest </h2>
+      <div className='flex flex-col items-center justify-center'>  
+      <div className=' max-w-xl flex flex-col  font-body'>
+        {/* heading */}
+<div className='flex flex-col gap-3 '>
+  <h2 className='font-semibold text-3xl font-body capitalize'>Start your <span className='italic text-transparent bg-clip-text  bg-gradient-to-br from-[#728ccd] to-[#364c84] font-[Merienda] '>learning journey</span> today</h2>
+  <p className='font-body max-w-lg'>Join thousands of learners building skills in tech, design, and business. Learn at your own pace with expert-led courses.</p>
 </div>
+{/* form */}
+<div className='py-6  w-10/12 '>
+  <div className=''>
+    <form action="" className='font-body flex flex-col gap-5'>
+      <div className='flex flex-col gap-1'>
+        <label htmlFor="Name" className='font-medium text-gray-800'>Full Name</label>
+        <input type="text" className='border rounded-lg  border-gray-400 p-3 placeholder:capitalize hover:border-[#364c84]  ' placeholder='Enter your full name'/>
+      </div>
+       <div className='flex flex-col gap-1'>
+        <label htmlFor="email" className='font-medium text-gray-800'>Email Address</label>
+        <input type="email" name='email' className='border rounded-lg  border-gray-400 p-3 placeholder:capitalize hover:border-[#364c84]  ' placeholder='Enter your email address'/>
+      </div>
+         <div className='flex flex-col gap-1'>
+        <label htmlFor="password" className='font-medium text-gray-800'>Password</label>
+        <input type="password"  name="password" className='border rounded-lg  border-gray-400 p-3 placeholder:capitalize hover:border-[#364c84]  ' placeholder='Enter your full Password'/>
+      </div>
+      <div className='flex flex-col gap-1'>
+        <label htmlFor="Confirmpassword" className='font-medium text-gray-800'>Confirm Password</label>
+        <input type="password"  name="Confirmpassword" className='border rounded-lg  border-gray-400 p-3 placeholder:capitalize hover:border-[#364c84]  ' placeholder='confirm  your  Password'/>
+      </div>
+      <div className='py-3'>
+       <button className='w-full transition-all bg-gradient-to-tr from-[#95b1ee] to-[#728ccd] font-body  cursor-pointer text-white rounded-xl py-2.5 px-5 text-lg box capitalize font-medium hover:scale-95'>Create Account</button>
+  </div>
+    </form>
+    <div className='flex flex-col'>
+    <div className='flex items-center justify-center gap-3 my-4'><span className='flex-1 h-px bg-gray-500'></span><span className='text-sm text-gray-700 capitalize'>or continue with</span><span className='flex-1 h-px bg-gray-500'></span></div>
+    </div>
+  </div>
+</div>
+      </div>
       </div>
       </div>
       </div>
